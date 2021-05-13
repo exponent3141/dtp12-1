b93db200cd01d94c4903e600997dd2ae7c918f32
exponent3141 <exponent3141@gmail.com>
2021-05-13 19:43:02 +1200

    fdd

--- main.py
+++ main.py
@@ -1,13 +1,73 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
+from forms import SearchForm
 
 
-app = Flask(__name__)
+
+import sqlite3
+
+app=Flask(__name__)
+
+app.config['SECRET_KEY'] = 'any secret string'
+
 
 
 @app.route('/')
+
 def home():
-  return render_template('home.html')
+
+  return render_template("home.html")
 
 
-if __name__ == '__main__':
+
+@app.route('/opening')
+
+def pizza(id):
+
+  conn = sqlite3.connect('chess_attempt.db')
+
+  cursor=conn.cursor()
+
+  cursor.execute('SELECT * FROM Main_Openings')
+
+  pizza=cursor.fetchall()
+
+  conn.close()
+
+  return render_template('pizza.html', pizza=pizza)
+
+
+
+@app.context_processor
+
+def inject_search():
+
+  searchform = SearchForm()
+
+  return dict(searchform=searchform)
+
+
+
+@app.route('/search', methods=['POST'])
+
+def search():
+
+  if request.method =='POST':
+
+    conn = sqlite3.connect('pizza.db')
+
+    cursor=conn.cursor()
+
+    c.executemany('''select * from Pizza where name = %s''', request.form['search'])
+
+    results = 'SELECT * FROM Pizza'
+
+    return render_template("results.html", records=c.fetchall())
+
+  return render_template('search.html')
+
+
+
+if __name__ == "__main__":
+
   app.run()
