c67625ce29fbaf591d271c1887a6eba1410db01f
exponent3141 <exponent3141@gmail.com>
2021-07-08 19:27:50 +1200

    made it not look bad and fixced table

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
 
 from forms import SearchForm
+import os.path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "chess_attempt.db")
 
 
 
@@ -27,7 +30,7 @@ def about():
 
 def Explorer():
 
-  conn = sqlite3.connect('chess_attempt.db')
+  conn = sqlite3.connect('wack.db')
 
   cursor=conn.cursor()
 
@@ -73,4 +76,4 @@ def search():
 
 if __name__ == "__main__":
 
-  app.run()
+  app.run(debug=True)
